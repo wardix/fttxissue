d880f8a1024044c9da042c22653efebb45e1ca43
Wardi <wardi@nusa.net.id>
2023-08-19 21:30:20 +0700

    refactor fttxIssueRoute

--- src/fttx-issue.route.ts
+++ src/fttx-issue.route.ts
@@ -1,101 +1,138 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { OkPacket, RowDataPacket } from "mysql2";
+
+interface RequestBody {
+  subject: string;
+  effect: string;
+  startTime: string;
+  operator: string;
+  operators: string[];
+  branch: string;
+  pop: string;
+  cids: string[];
+}
+
+interface SQLResult extends OkPacket {
+  insertId: number;
+}
+
+interface Row extends RowDataPacket {
+  cid: string;
+  csid: string;
+  acc: string;
+  hp: string;
+}
+
+const DEFAULT_ISSUE_STATUS = "Open";
+const DEFAULT_ISSUE_CAUSE = "under investigate";
+const DEFAULT_ISSUE_EMPLOYEE = "0200306";
+const DEFAULT_ISSUE_EFFECT = "Ya";
+const CID_ATTRIBUTE = "Vendor CID";
+const FTTX_TYPE = "fttx";
 
 export const fttxIssueRoute = async (fastify: FastifyInstance) => {
-  fastify.post("/", async (req: any, _reply) => {
-    const connection = await fastify.mysql.getConnection();
+  fastify.post(
+    "/",
+    async (req: FastifyRequest<{ Body: RequestBody }>, reply: FastifyReply) => {
+      const connection = await fastify.mysql.getConnection();
 
-    const {
-      subject,
-      effect,
-      startTime,
-      operator,
-      operators,
-      branch,
-      pop,
-      cids,
-    } = req.body;
+      try {
+        const {
+          subject,
+          effect,
+          startTime,
+          operator,
+          operators,
+          branch,
+          pop,
+          cids,
+        } = req.body;
 
-    const [result, _] = await connection.query(
-      `
-      INSERT INTO noc (
-        start_time,
-        end_time,
-        subject,
-        status,
-        cause,
-        effect,
-        eksternal,
-        branchId,
-        employee_id,
-        datetime,
-        fo_vendor_id,
-        fiber_vendor_id,
-        effected_customer,
-        type
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)`,
-      [
-        startTime,
-        startTime,
-        subject,
-        "Open",
-        "under investigate",
-        effect,
-        effect,
-        branch,
-        "0200306",
-        operator,
-        pop,
-        "Ya",
-        "fttx",
-      ]
-    );
+        const [result] = await connection.query<SQLResult>(
+          `
+        INSERT INTO noc (
+          start_time,
+          end_time,
+          subject,
+          status,
+          cause,
+          effect,
+          eksternal,
+          branchId,
+          employee_id,
+          datetime,
+          fo_vendor_id,
+          fiber_vendor_id,
+          effected_customer,
+          type
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)`,
+          [
+            startTime,
+            startTime,
+            subject,
+            DEFAULT_ISSUE_STATUS,
+            DEFAULT_ISSUE_CAUSE,
+            effect,
+            effect,
+            branch,
+            DEFAULT_ISSUE_EMPLOYEE,
+            operator,
+            pop,
+            DEFAULT_ISSUE_EFFECT,
+            FTTX_TYPE,
+          ]
+        );
 
-    const insertId = (result as any).insertId;
+        const insertId = result.insertId;
 
-    const operatorsPlaceHolder = new Array(operators.length)
-      .fill("?")
-      .join(", ");
-    const cidsPlaceHolder = new Array(cids.length).fill("?").join(", ");
+        const operatorsPlaceHolder = new Array(operators.length)
+          .fill("?")
+          .join(", ");
+        const cidsPlaceHolder = new Array(cids.length).fill("?").join(", ");
 
-    const [rows, _fields] = await connection.query(
-      `
-      SELECT cstc.value cid, cs.CustServId csid, cs.CustAccName acc, e.EmpHP hp
-      FROM CustomerServiceTechnicalCustom cstc
-      LEFT JOIN CustomerServiceTechnicalLink cstl
-        ON cstc.technicalTypeId = cstl.id 
-      LEFT JOIN noc_fiber nf ON cstl.foVendorId = nf.id 
-      LEFT JOIN CustomerServices cs ON cstl.CustServId = cs.custServId 
-      LEFT JOIN fiber_vendor fv ON nf.vendorId =  fv.id 
-      LEFT JOIN Customer c ON cs.CustId = c.CustId  
-      LEFT JOIN Employee e ON e.EmpId = c.SalesId 
-      WHERE cstc.attribute = 'Vendor CID' 
-        AND fv.id IN (${operatorsPlaceHolder})
-        AND cstc.value IN (${cidsPlaceHolder})`,
-      [...operators, ...cids]
-    );
+        const [rows] = await connection.query<Row[]>(
+          `
+        SELECT cstc.value cid, cs.CustServId csid, cs.CustAccName acc, e.EmpHP hp
+        FROM CustomerServiceTechnicalCustom cstc
+        LEFT JOIN CustomerServiceTechnicalLink cstl ON cstc.technicalTypeId = cstl.id
+        LEFT JOIN noc_fiber nf ON cstl.foVendorId = nf.id
+        LEFT JOIN CustomerServices cs ON cstl.CustServId = cs.custServId
+        LEFT JOIN fiber_vendor fv ON nf.vendorId =  fv.id
+        LEFT JOIN Customer c ON cs.CustId = c.CustId
+        LEFT JOIN Employee e ON e.EmpId = c.SalesId
+        WHERE cstc.attribute = ?
+          AND fv.id IN (${operatorsPlaceHolder})
+          AND cstc.value IN (${cidsPlaceHolder})`,
+          [CID_ATTRIBUTE, ...operators, ...cids]
+        );
 
-    const notification: any = {};
-    const subscription: any = {};
-    for (const id in rows) {
-      const { cid, csid, acc, hp } = (rows as any)[id];
+        const notification: Record<string, any[]> = {};
+        const subscription: Record<string, any[]> = {};
 
-      if (!notification.hasOwnProperty(hp)) {
-        notification[hp] = [];
+        const promises = rows.map((row) => {
+          const { cid, csid, acc, hp } = row;
+
+          notification[hp] = notification[hp] ?? [];
+          notification[hp].push({ csid, acc });
+
+          subscription[cid] = subscription[cid] ?? [];
+          subscription[cid].push({ csid, acc });
+
+          return connection.query(
+            "INSERT INTO noc_customer_service (noc_id, cs_id) VALUES (?, ?)",
+            [insertId, csid]
+          );
+        });
+
+        await Promise.all(promises);
+
+        reply.send({ data: subscription });
+      } catch (error) {
+        console.error(error);
+        reply.code(500).send({ error: "Database operation failed." });
+      } finally {
+        connection.release();
       }
-      notification[hp].push({ csid, acc });
-
-      if (!subscription.hasOwnProperty(cid)) {
-        subscription[cid] = [];
-      }
-      subscription[cid].push({ csid, acc });
-      connection.query(
-        "INSERT INTO noc_customer_service (noc_id, cs_id) VALUES (?, ?)",
-        [insertId, csid]
-      );
     }
-
-    connection.release();
-
-    return { data: subscription };
-  });
+  );
 };
